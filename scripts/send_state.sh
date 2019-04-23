0ae0e79dfa08a86021dc12310fdae4f53053066b
Brett <brettgraham@gmail.com>
2019-04-23 08:25:55 -0400

    send disk usage information about worker video directories in state message

--- scripts/send_state.sh
+++ scripts/send_state.sh
@@ -3,9 +3,12 @@
 # source configuration
 source /home/pi/scripts/config.sh
 
+# get space left in video directory
+DF=`df -h $VIDEO_DIR | tail -n 1 | awk '{print $2,$3,$5}'`
+
 # make state message
 HOSTNAME=`hostname`
-MSG="{\"hostname\": \"$HOSTNAME\", \"state\": \"`cat $STATE_FILE`\"}"
+MSG="{\"hostname\": \"$HOSTNAME\", \"state\": \"`cat $STATE_FILE`\", \"df\": \"$DF\"}"
 
 # send state message to queen
 echo "Sending $MSG"
