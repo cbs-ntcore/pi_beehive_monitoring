0c60429acb626e15e7ed70a93b0975d39919d31a
brett <brettgraham@gmail.com>
2019-07-09 13:31:13 -0400

    record settings from rig

--- scripts/record.sh
+++ scripts/record.sh
@@ -28,7 +28,7 @@ echo "Recording to $FN"
 bash /home/pi/scripts/purge_videos.sh
 
 # record for 20 seconds
-raspivid -o $TEMP_FN -t 20000 -n -fps 3 -md 2
+raspivid -o $TEMP_FN -t 20000 -n -fps 3 -md 2 -co 25 -ag 1.5 -br 45 -awb cloud
 
 # move temp file to videos directory
 mv $TEMP_FN $FN
